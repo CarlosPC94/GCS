7f5fa90a5c6e8d08b83692b7b441fec8051ad827
CarlosPC94 <cpc73@alu.ua.es>
2022-05-21 13:52:32 +0200

    pagina de login creada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
+/*   {
+    path: '',
+    redirectTo: 'folder/Inbox',
+    pathMatch: 'full'
+  }, */
   {
     path: '',
-    redirectTo: 'login',
-    pathMatch: 'full'
+    component: LoginComponent
   },
   {
     path: 'folder/:id',
